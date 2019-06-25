fd3b32170bb6b6c0924f258a6f6646cda31bf74a
thaycacac <thaycacac@gmail.com>
2019-06-25 14:40:05 +0700

    update: footer space

--- src/space/FooterSpace.tsx
+++ src/space/FooterSpace.tsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
 class FooterSpace extends Component<any> {
   render() {
     return (
-      <div>Footer Space</div>
+      <WrapAll>
+        <div>Copyright © 2019</div>
+      </WrapAll>
     )
   }
-} 
+}
+
+const WrapAll = styled.div`
+  height: 20px;
+  background: #9f60ff;
+  color: #dcd3ff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
 export default FooterSpace
