972e0fbf73f6a4f04311a1551b2bb2548b17867c
thaycacac <thaycacac@gmail.com>
2019-06-28 08:49:00 +0700

    add: text in element space

--- src/element-space/text/index.tsx
+++ src/element-space/text/index.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import { Subscribe } from 'unstated'
 
 interface IText {
   value: string,
@@ -17,7 +18,41 @@ class Text extends Component<IText> {
 
   static Inspector(container: any) {
     return {
-      general: 
+      general:
+        <Subscribe to={[container]}>
+          {
+            () => {
+              const { value } = container.state
+              return <input type='text' value={value} />
+            }
+          }
+        </Subscribe>
     }
   }
+
+  componentDidMount() {
+    console.log('refText', this.refText);
+  }
+
+  onChangeText = (event: any) => {
+    const value = event.target.value
+    console.log('event target of text', value);
+    this.props.elementContainer.setState({ value })
+  }
+
+  render() {
+    console.log('props text ', this.props);
+    return (
+      <Span
+        contentEditable
+        onInput={(e) => this.onChangeText(e)}
+      >
+        { this.props.value }
+      </Span>
+    )
+  }
 }
+
+const Span = styled.span`
+  color: red
+`
