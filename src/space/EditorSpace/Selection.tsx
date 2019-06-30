d9a1a507188caa6b287ea79d8e783e397a5005eb
thaycacac <thaycacac@gmail.com>
2019-06-30 15:16:16 +0700

    use: hook for section

--- src/space/EditorSpace/Selection.tsx
+++ src/space/EditorSpace/Selection.tsx
@@ -1,41 +1,37 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { Subscribe } from 'unstated'
 import { EditorSpaceContainer, StoreElement }from '../../container'
 
-class Selection extends Component<any> {
-  refSel!: HTMLElement
+function Selection(props: any) {
+  let refSel!: HTMLElement
 
-  updatePosition = () => {
-    const { selectedId } = this.props
-    const target: any = document.querySelector(`[data-element="${selectedId}"]`)
-    const { width, height, top, left } = target.getBoundingClientRect()
-    const scrollTop = window.scrollY
-    Object.assign(this.refSel.style, {
-      width: width + 'px',
-      height: height + 'px',
-      top: top +scrollTop+ 'px',
-      left: left + 'px',
-      display: 'block'
-    })
-  }
+  useEffect(() => {
+    if (props.selectedId) {
+      const { selectedId } = props
+      const target: any = document.querySelector(`[data-element="${selectedId}"]`)
+      const { width, height, top, left } = target.getBoundingClientRect()
+      const scrollTop = window.scrollY
+      Object.assign(refSel.style, {
+        width: width + 'px',
+        height: height + 'px',
+        top: top +scrollTop+ 'px',
+        left: left + 'px',
+        display: 'block'
+      })
+    }
+  }, [props.selectedId])
 
-  componentDidUpdate(){
-    this.updatePosition()
-  }
-
-  render() {
-    return (
-      <Subscribe to={[EditorSpaceContainer]}>
-        {
-          () => {
-            const { selected } = EditorSpaceContainer.state
-            return <UISelection ref={ e => this.refSel = e as HTMLInputElement}/>
-          }
+  return (
+    <Subscribe to={[EditorSpaceContainer]}>
+      {
+        () => {
+          const { selected } = EditorSpaceContainer.state
+          return <UISelection ref={ e => refSel = e as HTMLInputElement}/>
         }
-      </Subscribe>
-    )
-  }
+      }
+    </Subscribe>
+  )
 }
 
 const UISelection = styled.div`
