2b74f5c3063106ac21e775c12be29cea87cb7ed2
thaycacac | phatto pham <thaycacac@gmail.com>
2019-07-23 02:45:08 -0700

    update: json to dom

--- src/utils/convertJsonToDom.tsx
+++ src/utils/convertJsonToDom.tsx
@@ -2,20 +2,25 @@ function convertJsonToDom(dom: any) {
   let node = null
   const nodeType = dom.nodeType
 
-  if (nodeType === 1) {
-    const childNodes = dom.childNodes
-    const len = childNodes.length
-    node = document.createElement('ul')
-    if (len !== 0) {
-      node.classList.add('nested')
-    }
-    const text = document.createTextNode(dom.tagName)
-    node.appendChild(text)
+  switch (nodeType) {
+    case 1: //ELEMENT_NODE
+      const childNodes = dom.childNodes
+      const len = childNodes.length
+      node = document.createElement('ul')
+      if (len !== 0) {
+        node.classList.add('nested')
+      }
+      const text = document.createTextNode(dom.tagName)
+      node.appendChild(text)
 
-    for (let i = 0; i < len; i++) {
-      // @ts-ignore
-      if (childNodes[i]) node.appendChild(convertJsonToDom(childNodes[i]))
-    }
+      for (let i = 0; i < len; i++) {
+        // @ts-ignore
+        if (childNodes[i]) node.appendChild(convertJsonToDom(childNodes[i]))
+      }
+      break
+    case 3: //TEXT_NODE
+      node = document.createTextNode('')
+      break
   }
   return node
 }
