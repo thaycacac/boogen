f138b69d7ed37130be508d3e5f792bb3700569b0
thaycacac | phatto pham <thaycacac@gmail.com>
2019-09-05 08:53:24 -0700

    update: element preview

--- src/utils/elementData.tsx
+++ src/utils/elementData.tsx
@@ -82,6 +82,134 @@ const elementData = [
       childrend: [],
     },
   ],
+  [
+    {
+      id: 1,
+      type: 'Collapse',
+      name: 'Collapse',
+      childrend: [],
+    },
+  ],
+  [
+    {
+      id: 1,
+      type: 'Dropdowns',
+      name: 'Dropdowns',
+      childrend: [],
+    },
+  ],
+  [
+    {
+      id: 1,
+      type: 'Forms',
+      name: 'Forms',
+      childrend: [],
+    },
+  ],
+  [
+    {
+      id: 1,
+      type: 'InputGroup',
+      name: 'Input Group',
+      childrend: [],
+    },
+  ],
+  [
+    {
+      id: 1,
+      type: 'Jumbotron',
+      name: 'Jumbotron',
+      childrend: [],
+    },
+  ],
+  [
+    {
+      id: 1,
+      type: 'ListGroup',
+      name: 'List Group',
+      childrend: [],
+    },
+  ],
+  [
+    {
+      id: 1,
+      type: 'MediaObject',
+      name: 'Media Object',
+      childrend: [],
+    },
+  ],
+  [
+    {
+      id: 1,
+      type: 'Modal',
+      name: 'Modal',
+      childrend: [],
+    },
+  ],
+  [
+    {
+      id: 1,
+      type: 'Navs',
+      name: 'Navs',
+      childrend: [],
+    },
+  ],
+  [
+    {
+      id: 1,
+      type: 'Navbar',
+      name: 'Navbar',
+      childrend: [],
+    },
+  ],
+  [
+    {
+      id: 1,
+      type: 'Pagination',
+      name: 'Pagination',
+      childrend: [],
+    },
+  ],
+  [
+    {
+      id: 1,
+      type: 'Popovers',
+      name: 'Popovers',
+      childrend: [],
+    },
+  ],
+  [
+    {
+      id: 1,
+      type: 'Progress',
+      name: 'Progress',
+      childrend: [],
+    },
+  ],
+  [
+    {
+      id: 1,
+      type: 'Spinners',
+      name: 'Spinners',
+      childrend: [],
+    },
+  ],
+  [
+    {
+      id: 1,
+      type: 'Toasts',
+      name: 'Toasts',
+      childrend: [],
+    },
+  ],
+  [
+    {
+      id: 1,
+      type: 'Tooltips',
+      name: 'Tooltips',
+      childrend: [],
+    },
+  ],
 ]
 
 export default elementData
